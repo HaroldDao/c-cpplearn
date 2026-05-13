ad9346f23adab32afa2bc684dee18355e8cf978d
HaroldDao <kseven.duyphuc@gmail.com>
2026-05-14 06:30:50 +0700

    re-commit 1405

---
+++ looptest/lkt/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    do {
+        if (i % 2 == 0) {
+            cout << "Hello " << i << "\n";
+        }
+        i += 1;
+    } while (i < 10);
+}
